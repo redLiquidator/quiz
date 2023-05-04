13d114ac9315ba1844f319870b9a1aeb825ae045
benjamin <inufuni@naver.com>
2023-05-04 12:01:35 -0700

    spring security removed, localhost:8180 for accessing index.html, created main controller file

--- quiz/src/main/java/com/example/demo/controller/Quiz.java
+++ quiz/src/main/java/com/example/demo/controller/Quiz.java
@@ -1,12 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("rest/users")
-public class UsersResources {
-	
-
-	
+@Controller
+@RequestMapping("quiz")
+public class Quiz{
+	public String index() {
+		return "index";
+	}
 }
