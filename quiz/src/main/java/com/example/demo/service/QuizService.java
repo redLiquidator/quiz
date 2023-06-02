b68df66640ac70d1b3a29a151a67c6d43ec714a2
benjamin <inufuni@naver.com>
2023-06-01 21:58:41 -0700

    MyAccount ->myAccount

---
+++ quiz/src/main/java/com/example/demo/service/QuizService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Quiz;
+
+//@Autowired
+@Service
+public class QuizService {
+	//List<Quiz> SelectQuizList(){
+		//return quizMapper.SelectQuizList();
+	//}
+}
